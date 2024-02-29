8baa464feab414f562d0f5d6270ba30d407e1ed1
tujal.logicalstreet <tujal.logicalstreet@gmail.com>
2024-02-29 15:46:49 +0530

    fix some issue

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ApplicationRecord
     has_one_attached :profile_image
+    # after_create :send_welcome_email
+
+    # private
+    #  def send_welcome_email
+    #     WelcomeEmailJob.perform_now(self)
+    #  end
 end
