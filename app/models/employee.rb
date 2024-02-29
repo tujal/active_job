9b8c487e95c4176bd4f731465d1896e26e812854
tujal.logicalstreet <tujal.logicalstreet@gmail.com>
2024-02-29 16:07:27 +0530

    validation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,9 +1,4 @@
 class Employee < ApplicationRecord
     has_one_attached :profile_image
-    # after_create :send_welcome_email
-
-    # private
-    #  def send_welcome_email
-    #     WelcomeEmailJob.perform_now(self)
-    #  end
+    validates :name, :joining_date, :email, :company_name, :position, presence: true
 end
