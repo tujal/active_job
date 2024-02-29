12f7f4b4da03602c3b901635cebcf11f29d91cf2
tujal.logicalstreet <tujal.logicalstreet@gmail.com>
2024-02-29 16:16:37 +0530

    image validation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,17 @@
 class Employee < ApplicationRecord
     has_one_attached :profile_image
     validates :name, :joining_date, :email, :company_name, :position, presence: true
+    validate :check_image_type
+
+    private
+    def check_image_type
+        if profile_image.attached?
+            if profile_image.content_type == "application/pdf"
+                errors.add(:profile_image , "Plz upload jpeg, png images ")
+            end
+        else
+            errors.add(:profile_image, "Image can't be blank")
+        end
+    end
+
 end
